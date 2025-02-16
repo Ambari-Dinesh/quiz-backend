b785fb071eb408ba5826a6b4be44c8d6b6deadaa
Ambari-Dinesh <ambaridinesh@gmail.com>
2025-02-16 15:43:54 +0530

    Initial commit

--- src/server.js
+++ src/server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 3000;
 createPool();
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}/api`);
 });
